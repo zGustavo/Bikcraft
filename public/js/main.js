7f469b168a3427c12828d38ab070f6465b46f93e
Gustavo Dias <gustavo.diias1@gmail.com>
2021-02-24 16:48:10 -0300

    add plugin slide responsive JQuery

---
+++ public/js/main.js
@@ -0,0 +1,5 @@
+$(".rslides").responsiveSlides({
+    auto: true,             // Boolean: Animate automatically, true or false
+    speed: 500,            // Integer: Speed of the transition, in milliseconds
+    timeout: 2000,          // Integer: Time between slide transitions, in milliseconds
+  });
