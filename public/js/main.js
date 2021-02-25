f4d69d0d26bbfb3483057b5e64304e7ab8a274d4
Gustavo Dias <gustavo.diias1@gmail.com>
2021-02-24 23:53:14 -0300

    Add animações css/js em todas as paginas FadeInDown

--- public/js/main.js
+++ public/js/main.js
@@ -10,3 +10,28 @@ $(".rslides").responsiveSlides({
     timeout: 4000,          // Integer: Time between slide transitions, in milliseconds
     pager: true,           // Boolean: Show pager, true or false
   });
+
+  Visibility.onVisible (function() {
+    setTimeout(function () {
+      $(".introducao h1").addClass("animate__animated animate__fadeInDown");
+    }, 400);
+    setTimeout(function () {
+      $(".introducao blockquote").addClass("animate__animated animate__fadeInDown");
+    }, 800);
+    setTimeout(function () {
+      $(".introducao .btn").addClass("animate__animated animate__fadeInDown");
+    }, 1200);
+    setTimeout(function () {
+      $(".animar").addClass("animate__animated animate__fadeInDown");
+    }, 1600);
+
+    setTimeout(function () {
+      $(".introducao_interna h1").addClass("animate__animated animate__fadeInDown");
+    }, 400);
+    setTimeout(function () {
+      $(".introducao_interna p").addClass("animate__animated animate__fadeInDown");
+    }, 800);
+    setTimeout(function () {
+      $(".animar-interno").addClass("animate__animated animate__fadeInDown");
+    }, 1200);
+  });
